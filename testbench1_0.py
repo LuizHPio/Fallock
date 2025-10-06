5260213650c38ed359f6a030298eadf4e5b3a9c1
LuizHPio <luizxgold@gmail.com>
2025-10-06 16:48:58 -0300

    fix: enhance test output message for clarity

--- testbench1_0.py
+++ testbench1_0.py
@@ -3,7 +3,7 @@ from tests.vector2Tests import Vector2Tester
 
 def main_thread():
     tester = Vector2Tester()
-    print(f'Test 01 status: {tester.test01()}')
+    print(f'Test 01 status: {"Success" if tester.test01() else "Failure"}')
 
 
 if __name__ == "__main__":
