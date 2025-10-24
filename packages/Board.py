ef287be8d7add8c6b6b316cdce9c4a64ddba120e
LuizHPio <luizxgold@gmail.com>
2025-10-24 18:24:57 -0300

    feat: enhance Board and GameManager functionality; implement physics logic, scoring, and movement handling

--- packages/Board.py
+++ packages/Board.py
@@ -2,18 +2,31 @@ from packages.Block import Block
 from packages.Piece import Piece
 from packages.Vector2 import Vector2
 from packages.InputHandler import Command
+from packages.Player import Player
 
 
 class Board:
     player_piece: Piece
+    player_manager: Player
     grid: list[list[Block | None]]
     height: int
     width: int
+    collapse_height: int
+    scan_height: int
+    is_falling_blocks: bool
+    is_animating: bool
+    blocks_fell_in_scan: bool
 
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, player_manager: Player):
+        self.player_manager = player_manager
         self.height = height
         self.width = width
         self.grid = []
+        self.is_animating = False
+        self.is_falling_blocks = False
+        self.blocks_fell_in_scan = False
+        self.collapse_height = -1
+        self.scan_height = -1
         self.generate_piece()
 
         for _ in range(self.width):
@@ -55,12 +68,82 @@ class Board:
             self.player_piece.origin.x -= 1
             return
 
-    def fall(self):
+        # implement function to prevent piece from moving outside of the board
+
+    def physics_logic(self):
+        if self.is_animating:
+            if self.is_falling_blocks:
+                self.apply_block_gravity()
+                return
+
         if self.has_collided(self.player_piece):
             self.petrify_piece(self.player_piece)
+            self.score_line()
             return
 
         self.player_piece.origin.y += 1
 
     def score_line(self):
-        pass
+        full_lines: list[int] = []
+
+        for y in range(self.height-1, -1, -1):
+            checking_line: list[Block | None] = []
+
+            for x in range(self.width):
+                checking_line.append(self.grid[x][y])
+
+            # line isnt full, continue
+            if None in checking_line:
+                continue
+
+            full_lines.append(y)
+
+        if full_lines:
+            for y in full_lines:
+                for x in range(self.width):
+                    self.grid[x][y] = None
+            self.is_animating = True
+            self.is_falling_blocks = True
+            self.collapse_height = max(full_lines)
+            self.scan_height = max(full_lines) - 1
+            self.blocks_fell_in_scan = False
+
+    def apply_block_gravity(self):
+        # i will first check from the collapse height and upwards(per line) if there is a block with an empty space below
+        # if i find a line with an empty space below i will move the blocks which can move, set the blocks_moved variable and then end the frame
+        # in the next frame i will do the same until the blocks_moved variable is not set
+
+        if self.scan_height < 0:
+            # full scan donew
+            if self.blocks_fell_in_scan:
+                self.blocks_fell_in_scan = False
+                self.scan_height = self.collapse_height - 1
+                return
+
+            self.collapse_height = -1
+            self.scan_height = -1
+            self.is_animating = False
+            self.is_falling_blocks = False
+            self.score_line()
+            return
+
+        for column in range(self.width):
+            if self.scan_height == -1 or self.collapse_height == -1:
+                return
+
+            if self.grid[column][self.scan_height] == None:
+                continue
+
+            # scan_height starts at index 0, sum one for index and one for below space
+            if self.scan_height + 1 < self.height:
+
+                matrix_space = self.grid[column][self.scan_height]
+                below_matrix_space = self.grid[column][self.scan_height+1]
+
+                if below_matrix_space == None:
+                    self.grid[column][self.scan_height +
+                                      1] = matrix_space
+                    self.grid[column][self.scan_height] = None
+                    self.blocks_fell_in_scan = True
+
+        self.scan_height -= 1
--- packages/GameManager.py
+++ packages/GameManager.py
@@ -2,6 +2,8 @@ from packages.Board import Board
 from packages.Renderer import Renderer
 from packages.Vector2 import Vector2
 from packages.InputHandler import InputHandler, Command
+from packages.Player import Player
+import os
 import time
 import math
 
@@ -14,11 +16,14 @@ class GameManager():
     target_tickrate: float
     target_framerate: int
     input_handler: InputHandler
+    player_manager: Player
 
-    def __init__(self):
-        board_dimensions = Vector2(10, 20)
-        self.renderer = Renderer(board_dimensions)
-        self.board = Board(board_dimensions.x, board_dimensions.y)
+    def __init__(self, debug: bool = False):
+        board_dimensions = Vector2(12, 20)
+        self.renderer = Renderer(board_dimensions, debug)
+        self.player_manager = Player()
+        self.board = Board(board_dimensions.x,
+                           board_dimensions.y, self.player_manager)
         self.input_handler = InputHandler()
         self.target_tickrate = 128
         self.target_framerate = 256
@@ -30,8 +35,29 @@ class GameManager():
         if user_input in movement_commands:
             self.board.movement(user_input)
 
+        if user_input == "DUMP":
+            del self.renderer
+            self.debug_print(self.board)
+            exit()
+
         if user_input == "ESCAPE":
             self.pause_game()
+
+    def debug_print(self, board: Board):
+        os.system("clear")
+        for y in range(board.height):
+            for x in range(board.width):
+                for block in self.board.player_piece.blocks_relative_pos:
+                    abs_block = self.board.player_piece.getBlockAbsPos(block)
+                    if abs_block.x == x and abs_block.y == y:
+                        print("X", end="")
+                        break
+                else:
+                    if board.grid[x][y] == None:
+                        print(" ", end="")
+                    else:
+                        print("X", end="")
+            print("|")
 
     def save_score(self):
         pass
@@ -42,15 +68,19 @@ class GameManager():
 
         tickrate_counter = 0
         while True:
-            if self.elapsed_time(logic_timer) > 1/self.target_tickrate:
-                if tickrate_counter == self.difficulty(self.level):
-                    self.board.fall()
-                    self.renderer.draw(self.board)
-                    tickrate_counter = 0
-                tickrate_counter += 1
-                logic_timer = time.time_ns()
+            if self.board.is_animating:
+                self.renderer.draw(self.board)
+                self.board.physics_logic()
+            else:
+                if self.elapsed_time(logic_timer) > 1/self.target_tickrate:
+                    if tickrate_counter == self.difficulty(self.level):
+                        self.board.physics_logic()
+                        tickrate_counter = 0
+                    tickrate_counter += 1
+                    logic_timer = time.time_ns()
 
             self.process_input(self.input_handler.get_command())
+            self.renderer.draw(self.board)
 
             self.wait_framerate(render_timer)
             render_timer = time.time_ns()
